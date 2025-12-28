8c08e832062b87fffa26b6f6ec7f341faaa94a0a
Chris Hudson <chris@xetius.com>
2025-12-28 16:45:10 +0000

    Saved old git config

---
+++ .config/nvim-old/lua/plugins/misc.lua
@@ -0,0 +1,69 @@
+-- Standalone plugins with less than 10 lines of config go here
+return {
+  {
+    -- Tmux & split window navigation
+    'christoomey/vim-tmux-navigator',
+    lazy = false,
+  },
+  {
+    -- Detect tabstop and shiftwidth automatically
+    'tpope/vim-sleuth',
+  },
+  {
+    -- Powerful Git integration for Vim
+    'tpope/vim-fugitive',
+  },
+  {
+    -- GitHub integration for vim-fugitive
+    'tpope/vim-rhubarb',
+  },
+  {
+    -- Hints keybinds
+    'folke/which-key.nvim',
+  },
+  {
+    -- Autoclose parentheses, brackets, quotes, etc.
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    opts = {},
+  },
+  {
+    -- Highlight todo, notes, etc in comments
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+  {
+    -- High-performance color highlighter
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end,
+  },
+  {
+    -- Git worktree management
+    'ThePrimeagen/git-worktree.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    config = function()
+      require('git-worktree').setup()
+      require('telescope').load_extension 'git_worktree'
+    end,
+  },
+  {
+    'folke/twilight.nvim',
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    -- Markdown preview in browser
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = 'cd app && npm install',
+  },
+}
