1f2bacdf46dfb8bcb724ec3b95e404424bd85546
Chris Hudson <chris@xetius.com>
2025-12-28 18:11:58 +0000

    Added claude and lazydev to nvim config

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -4,3 +4,4 @@ vim.keymap.set('n', '<leader>ff', '<cmd>Telescope file_files<cr>', { desc = 'Tel
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = 'Telescope Live Grep' })
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Telescope Buffers' })
 vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = 'Telescope Help Tags' })
+
