f324e169d34a4bb2f17c829db37437b8d1872ed8
Chris Hudson <chris@xetius.com>
2026-01-11 13:50:27 +0000

    Changed markdown-preview to peek

--- .config/nvim/lua/plugins/markdown-preview.lua
+++
@@ -1,7 +0,0 @@
-return {
-    -- Markdown preview in browser
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = 'cd app && npm install',
-}
